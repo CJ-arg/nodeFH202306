bb5dee3c9cf9312f24d4a450b070d70f713ffc1b
CJ-arg <janoncarlos@gmail.com>
2024-01-17 11:35:18 -0300

    yarg test

--- src/plugins/args.plugins.test.ts
+++ src/plugins/args.plugins.test.ts
@@ -5,7 +5,15 @@ const runCommand = async (args: string[]) => {
   const { yarg } = await import("./args.plugins");
   return yarg;
 };
+
 describe("Test  args.plugins.ts", () => {
+  const originalArgv = process.argv;
+
+  beforeEach(() => {
+    process.argv = originalArgv;
+    jest.resetModules();
+  });
+
   test("should return default values", async () => {
     const argv = await runCommand(["-b", "5"]);
 
@@ -16,6 +24,29 @@ describe("Test  args.plugins.ts", () => {
         s: false,
         n: "multiplication-table",
         d: "outputs",
+      })
+    );
+  });
+  test("should return custom values", async () => {
+    const argv = await runCommand([
+      "-b",
+      "5",
+      "-l",
+      "10",
+      "-s",
+      "-n",
+      "custom-name",
+      "-d",
+      "custom-dir",
+    ]);
+
+    expect(argv).toEqual(
+      expect.objectContaining({
+        b: 5,
+        l: 10,
+        s: true,
+        n: "custom-name",
+        d: "custom-dir",
       })
     );
   });
