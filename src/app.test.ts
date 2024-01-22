d751e31232045028d23dcfe38f15940a0aa76242
CJ-arg <janoncarlos@gmail.com>
2024-01-22 17:14:45 -0300

    yarg test

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,3 +1,5 @@
+import { ServerApp } from "./presentation/server-app";
+
 describe("Test App.ts", () => {
   test("should call Server.run with values", async () => {
     const serverRunMock = jest.fn();
